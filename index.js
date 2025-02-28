c2f47e4134de4688b3d9e35eab30804eb8227c12
Samuel Oluwatobi Oluwasegun <oluwasegunsam56@gmail.com>
2025-02-28 19:48:58 +0100

    add CORS middleware and format MongoDB connection

--- index.js
+++ index.js
@@ -14,9 +14,16 @@ app.use(express.json());
 
 // Routes
 app.use('/api/projects', projectRoutes);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI)
+mongoose
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
